501254896b80e9fcbd47e77e051d80ee7d8bb73b
flamingoen <obelth@gmail.com>
2018-04-30 11:03:18 +0200

    Added new policies

--- analyses/policies.fs
+++ analyses/policies.fs
@@ -1,18 +1,21 @@
 module Policies
 open Defines
 
-type rule = R_Pl | R_Mi | R_Zr | R_Even | R_Odd
+type rule = R_Pl | R_Mi | R_Zr | R_Even | R_Odd | R_Grt of int | R_Lt of int | R_Eq of int
 type policy = Policy of (string * rule)
 type policies = policy List
 
 let p_true s = true
 
 let ruleToString = function
-    | R_Pl -> "+"
-    | R_Mi -> "-"
-    | R_Zr -> "0"
-    | R_Odd -> "odd"
-    | R_Even -> "even"
+    | R_Pl -> "->+"
+    | R_Mi -> "->-"
+    | R_Zr -> "->0"
+    | R_Odd -> "->odd"
+    | R_Even -> "->even"
+    | R_Grt(n) -> ">"+(string n)
+    | R_Lt(n) -> "<"+(string n)
+    | R_Eq(n) -> "="+(string n)
 
 let policyToString = function
-    (var,rule) -> var+"->"+(ruleToString rule)
+    (var,rule) -> var+(ruleToString rule)
